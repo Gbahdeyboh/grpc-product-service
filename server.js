36f1a943dde388adf08062105431cd66a7dddf2a
Gbahdeyboh <gbahdeybohbello@gmail.com>
2024-01-23 14:23:42 +0100

    Wrote server side code for product service

--- server.js
+++ server.js
@@ -6,24 +6,108 @@ const grpcObject = grpc.loadPackageDefinition(packageDef);
 
 const productPackage = grpcObject.productPackage;
 
-function createProduct(call, callback) {
+const products = [
+    {
+        id: 1,
+        name: "Samsung",
+        price: 3000,
+        category: "Smartphones",
+        description: "A stunning phone case",
+    },
+    {
+        id: 2,
+        name: "Samsung",
+        price: 3000,
+        category: "Smartphones",
+        description: "A stunning phone case",
+    },
+    {
+        id: 3,
+        name: "Samsung",
+        price: 3000,
+        category: "Smartphones",
+        description: "A stunning phone case",
+    }
+];
 
+// const productSample = {
+//     id: 1,
+//     name: "Samsung",
+//     price: 3000,
+//     category: "Smartphones",
+//     description: "A stunning phone case",
+// };
+
+function createProduct(call, callback) {
+    const data = call.request; 
+
+    const newProductData = { ...data, id: products.length + 1, new: false };
+
+    products.push(newProductData);
+
+    console.log("Sent Product is => ", newProductData);
+    callback(null, newProductData);
 }
 
 function readProduct(call, callback){
+    const productId = call.request.id;
+    const selectedProduct = products.find(product => product.id === productId);
 
+    console.log("selectedProduct is => ", selectedProduct);
+
+    if(selectedProduct){
+        callback(null, selectedProduct);
+    }else {
+        callback({
+            code: grpc.status.NOT_FOUND,
+            details: "Could not find a product with the specified ID"
+        });
+    }
 }
 
 function readProducts(call, callback){
-    
+    callback(null, { products });
 }
 
 function updateProduct(call, callback){
+    const productInfo = call.request;
+
+    const productIndex = products.findIndex(product => product.id === productInfo.id);
+    if(!productIndex){
+        callback({
+            code: grpc.status.NOT_FOUND,
+            details: "Could not find a product with the specified ID to update"
+        });
+    }
+
+    const selectedProduct = products[productIndex];
+
+    const updatedProduct = {
+        id: selectedProduct.id,
+        name: productInfo.name ? productInfo.name : selectedProduct.name,
+        description: productInfo.description ? productInfo.description : selectedProduct.description,
+        price: productInfo.price ? productInfo.price : selectedProduct.price,
+        category: productInfo.category ? productInfo.category : selectedProduct.category,
+    }
+
+    products.splice(productIndex, 1, updatedProduct);
     
+    callback(null, updatedProduct);
 }
 
 function deleteProduct(call, callback){
-    
+    const productId = call.request.id;
+    const productIndex = products.findIndex(product => product.id === productId);
+    if(!productIndex){
+        callback({
+            code: grpc.status.NOT_FOUND,
+            details: "Could not find a product with the specified ID to delete"
+        });
+    }
+
+    products.splice(productIndex, 1);
+
+    callback(null, { deleted: true });
 }
 
 
