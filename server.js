412c41f77992d1ef51d30ec12025a645093841b4
Gbadebo Bello <gbahdeybohbello@gmail.com>
2024-02-26 19:41:24 +0100

    fixes

--- server.js
+++ server.js
@@ -4,56 +4,23 @@ const protoLoader = require("@grpc/proto-loader");
 const packageDef = protoLoader.loadSync("product.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDef);
 
-const productPackage = grpcObject.productPackage;
+const productPackage = grpcObject.product;
 
-const products = [
-    {
-        id: 1,
-        name: "Samsung",
-        price: 3000,
-        category: "Smartphones",
-        description: "A stunning phone case",
-    },
-    {
-        id: 2,
-        name: "Samsung",
-        price: 3000,
-        category: "Smartphones",
-        description: "A stunning phone case",
-    },
-    {
-        id: 3,
-        name: "Samsung",
-        price: 3000,
-        category: "Smartphones",
-        description: "A stunning phone case",
-    }
-];
-
-// const productSample = {
-//     id: 1,
-//     name: "Samsung",
-//     price: 3000,
-//     category: "Smartphones",
-//     description: "A stunning phone case",
-// };
+const products = [];
 
 function createProduct(call, callback) {
-    const data = call.request; 
+    const data = call.request;
 
     const newProductData = { ...data, id: products.length + 1 };
 
     products.push(newProductData);
 
-    console.log("Sent Product is => ", newProductData);
     callback(null, newProductData);
 }
 
 function readProduct(call, callback){
     const productId = call.request.id;
     const selectedProduct = products.find(product => product.id === productId);
-
-    console.log("selectedProduct is => ", selectedProduct);
 
     if(selectedProduct){
         callback(null, selectedProduct);
@@ -73,6 +40,7 @@ function updateProduct(call, callback){
     const productInfo = call.request;
 
     const productIndex = products.findIndex(product => product.id === productInfo.id);
+
     if(!productIndex){
         callback({
             code: grpc.status.NOT_FOUND,
@@ -81,15 +49,15 @@ function updateProduct(call, callback){
     }
 
     const selectedProduct = products[productIndex];
-
+    
     const updatedProduct = {
         id: selectedProduct.id,
-        name: productInfo.name ? productInfo.name : selectedProduct.name,
-        description: productInfo.description ? productInfo.description : selectedProduct.description,
-        price: productInfo.price ? productInfo.price : selectedProduct.price,
-        category: productInfo.category ? productInfo.category : selectedProduct.category,
+        name: productInfo.name ?? selectedProduct.name,
+        description: productInfo.description ?? selectedProduct.description,
+        price: productInfo.price ?? selectedProduct.price,
+        category: productInfo.category ?? selectedProduct.category,
     }
-
+    
     products.splice(productIndex, 1, updatedProduct);
     
     callback(null, updatedProduct);
@@ -111,17 +79,16 @@ function deleteProduct(call, callback){
 }
 
 
-(function(){
-    const server = new grpc.Server();
-    server.addService(productPackage.Todo.service, {
-        createProduct,
-        readProduct,
-        readProducts,
-        updateProduct,
-        deleteProduct,
-    });
+const server = new grpc.Server();
+server.addService(productPackage.Product.service, {
+    createProduct,
+    readProduct,
+    readProducts,
+    updateProduct,
+    deleteProduct,
+});
 
-    server.bindAsync("0.0.0.0:5000", grpc.ServerCredentials.createInsecure(), () => {
-        server.start();
-    });
-})();
+server.bindAsync("0.0.0.0:4000", grpc.ServerCredentials.createInsecure(), () => {
+    server.start();
+    console.log("Server Started!!!");
+});
